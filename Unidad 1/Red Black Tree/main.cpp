0437f655ca81f0f695f4ca4db879ccaac7da5a8f
2023JoshuaP <jphilco@unsa.edu.pe>
2024-10-01 09:03:30 -0500

    Contenido del mainprueba pasa al main principal.

--- Unidad 1/Red Black Tree/main.cpp
+++ Unidad 1/Red Black Tree/main.cpp
@@ -1,31 +1,35 @@
 #include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
 #include "./RedBlackTree.cpp"
 
 int main() {
-    RedBlackTree RBTree;
+    std::vector<int> keys;
+    std::srand(std::time(0));
 
-    RBTree.Insert(10);
-    RBTree.Insert(5);
-    RBTree.Insert(15);
-    RBTree.Insert(3);
-    RBTree.Insert(7);
-    RBTree.Insert(12);
-    RBTree.Insert(18);
-    RBTree.Insert(1);
-    
-    std::cout << "Estructura del Red Black Tree:" << std::endl;
-    RBTree.PrintTree();
+    for (int n = 100; n <= 10000; n *= 10) {
+        RedBlackTree RBTree;
+        keys.clear();
 
-    RBTree.generateDOT("RedBlackTree.dot");
-    
-    /*
-    RBTree.remove(5);
+        for (int i = 1; i <= n; i++) {
+            keys.push_back(i);
+            RBTree.Insert(i);
+        }
+        
+        int comparaciones_totales = 0;
+        int repeticiones = 100;
 
-    std::cout << "Despues de eliminar el nodo 5:" << std::endl;
-    RBTree.PrintTree();
-    */
+        for (int i = 0; i < repeticiones; i++) {
+            int claves_random = keys[std::rand() % n];
+            int comparaciones = 0;
+            RBTree.Search(claves_random, comparaciones);
+            comparaciones_totales += comparaciones;
+        }
 
-    std::cout << "Buscando nodo 15: " << (RBTree.Search(15) != RBTree.Search(0)) << std::endl;
-    std::cout << "Buscando nodo 25: " << (RBTree.Search(25) != RBTree.Search(0)) << std::endl;
+        double promedio_comparaciones = comparaciones_totales / (double)repeticiones;
+        std::cout << "Numero de claves: " << n << " | Comparaciones promedio: " << promedio_comparaciones << std::endl;
+    }
+
     return 0;
 }
